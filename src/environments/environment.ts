81191170e119cc10002608ac2fc034bd9b6cdf60
Patrick Berndt <122051407+PatrickBerndt@users.noreply.github.com>
2023-08-18 14:51:29 +0200

    added sample data and sort function to user component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,11 @@
 
+export const environment = {
+    firebase: {
+      projectId: 'simplecrm-dcda1',
+      appId: '1:516481619633:web:149e2d5553cc401d8b3490',
+      storageBucket: 'simplecrm-dcda1.appspot.com',
+      apiKey: 'AIzaSyALnv6yNtFcS7UiuqOFsrfT1aNaCFvLOsU',
+      authDomain: 'simplecrm-dcda1.firebaseapp.com',
+      messagingSenderId: '516481619633',
+    },
+  };
